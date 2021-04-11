9fe6bb313b88d0f8af0cadd84a6e9d867a25cc5c
Ubuntu <loeza.carlos@yahoo.com>
2021-04-11 16:19:21 -0700

    Part 2 and 3 complete

---
+++ turnin/cloez001_lab3_part3.c
@@ -0,0 +1,52 @@
+
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    unsigned char tmpC = 0;
+    unsigned char fuel = 0;
+    unsigned char tmp = 0;
+
+    /* Insert your solution below */
+    while (1) {
+	fuel = PINA & 0x0F;
+	if(fuel == 0){
+	    tmpC = 0x40;
+	}else if (fuel == 0x01 || fuel == 0x02){
+	    tmpC = 0x60;
+	} else if (fuel == 0x03 || fuel == 0x04){
+	    tmpC = 0xE0;
+	} else if (fuel == 0x05 || fuel == 0x06){
+	    tmpC = 0x38;
+	} else if (fuel > 0x06 && fuel < 0x0A) {
+	    tmpC = 0x3C;
+	} else if (fuel > 0x09 && fuel < 0x0D) {
+	    tmpC = 0x3E;
+	} else 
+	    tmpC = 0x3F;
+
+	tmp = PINA & 0xF0;
+	// part 2
+	if(tmp ==0x30){
+	    tmpC = tmpC | 0x80;
+	}else 
+	    tmpC = tmpC | tmp;
+
+	PORTC = tmpC;
+    }
+    
+}
